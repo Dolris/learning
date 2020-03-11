40538e1e463627745133f5beb695da123781d3f7
Dolris <57836410+Dolris@users.noreply.github.com>
2020-03-11 19:24:28 +0200

    Delete Task23_2.java

--- com/alpha/Task23_2.java
+++
@@ -1,9 +0,0 @@
-package com.alpha;
-import com.alpha.work2a.Runner;
-
-public class Task23_2 {
-    public static void main(String[] args) {
-        Runner r = new Runner();
-        r.run();
-    }
-}
