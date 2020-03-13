4c5cb6ec24e32bcfd72cbdde3e1f95209255f323
Dolris <57836410+Dolris@users.noreply.github.com>
2020-03-13 20:32:55 +0200

    Homework #2_8

--- src/com/codespace/task3_4/MyMixer.java
+++ src/com/codespace/task3_4/MyMixer.java
@@ -1,4 +1,22 @@
 package com.codespace.task3_4;
 
+import java.util.ArrayList;
+
 public class MyMixer<T> {
+    public ArrayList array = new ArrayList();
+    public MyMixer() {}
+    public void shuffle() {
+        int e;
+        T el;
+        if (array.isEmpty()) return;
+        for (int i = 0; i < array.size() ; i++) {
+            e = (int)(array.size() * Math.random());
+            if (i != e) {
+                el = (T)(array.get(i));
+                array.set(i, array.get(e));
+                array.set(e, el);
+            }
+        }
+    }
+
 }
--- src/com/codespace/task3_4/Runner.java
+++ src/com/codespace/task3_4/Runner.java
@@ -1,5 +1,27 @@
 package com.codespace.task3_4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Runner {
-    public void run() {}
+    public void run() {
+        MyMixer<String> s = new MyMixer<String>();
+        s.array.add("AAA");
+        s.array.add("BBB");
+        s.array.add("CCC");
+        s.array.add("DDD");
+        s.array.add("EEE");
+        MyMixer<Integer> i = new MyMixer<Integer>();
+        i.array.add(1);
+        i.array.add(2);
+        i.array.add(3);
+        i.array.add(4);
+        i.array.add(5);
+        System.out.println("Strings: " + s.array.toString());
+        System.out.println("Integers: " + i.array.toString());
+        s.shuffle();
+        i.shuffle();
+        System.out.println("Strings: " + s.array.toString());
+        System.out.println("Integers: " + i.array.toString());
+    }
 }
