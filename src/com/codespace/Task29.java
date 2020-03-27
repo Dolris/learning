82e4ea9994b65ad6b7676aa4cb898e4264d3275c
dolris <noname>
2020-03-27 19:15:40 +0200

    Task 29 begin

---
+++ src/com/codespace/Task29.java
@@ -0,0 +1,10 @@
+package com.codespace;
+
+import com.codespace.task1_29.Runner;
+
+public class Task29 {
+    public static void main(String[] args) {
+        Runner r = new Runner();
+        r.run();
+    }
+}
---
+++ src/com/codespace/task1_29/Runner.java
@@ -0,0 +1,7 @@
+package com.codespace.task1_29;
+
+public class Runner {
+    public void run () {
+        System.out.println("sddfgh");
+    }
+}
